338c7e09525b55e1adbda3882c4b5d23b766c145
Víctor Guzmán <vgzman@gmail.com>
2019-07-16 23:17:44 +0200

    Find unused barcodes from the list

--- main.py
+++ main.py
@@ -2,8 +2,33 @@
 import csv
 
 def main():
-    customer_orders = dict()
+    unique_barcodes = set()
+    duplicated_barcodes = set()
+    unused_barcodes = set()
     order_barcode = dict()
+
+
+    with open('barcodes.csv') as csv_file:
+        barcodes = csv.DictReader(csv_file, delimiter=',')
+        for row_barcode in barcodes:
+            barcode = row_barcode['barcode']
+            order = row_barcode['order_id']
+            
+            if barcode not in unique_barcodes:
+                unique_barcodes.add(barcode)
+            else:
+                unique_barcodes.remove(barcode)
+                duplicated_barcodes.add(barcode)
+
+            if order:
+                if order not in order_barcode.keys():
+                    order_barcode[order] = []
+                order_barcode[order].append(barcode)
+            else:
+                unused_barcodes.add(barcode)
+
+
+    customer_orders = dict()
     
     with open('orders.csv') as csv_file_2:
         orders = csv.DictReader(csv_file_2, delimiter=',')
@@ -12,12 +37,7 @@ def main():
                 customer_orders[row_order['customer_id']] = []
             customer_orders[row_order['customer_id']].append(row_order['order_id'])
     
-    with open('barcodes.csv') as csv_file:
-        barcodes = csv.DictReader(csv_file, delimiter=',')
-        for row_barcode in barcodes:
-            if row_barcode['order_id'] not in order_barcode.keys():
-                order_barcode[row_barcode['order_id']] = []
-            order_barcode[row_barcode['order_id']].append(row_barcode['barcode'])
+    
     
     customer_order_barcode = dict()
     for customer, orders in customer_orders.items():
